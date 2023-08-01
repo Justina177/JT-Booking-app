7294aac01c919a0a1020e4d1db6b6d0f6152923a
justina177 <justinaenenche@yahoo.com>
2023-08-02 00:50:59 +0100

    add countBytype route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import hotelsRoute from "./routes/hotels.js"
-import roomsRoute from "./routes/rooms.js"
-import authRoute from "./routes/auth.js"
-import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
 import cookieParser from "cookie-parser";
+// import cors from "cors";
+
 
 const app = express();
 dotenv.config()
