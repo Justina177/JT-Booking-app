9de7f0644524e073b89b0fd5922ba5f5724fcb55
justina177 <justinaenenche@yahoo.com>
2023-08-13 17:36:05 +0100

    Added function to hanndle onClick function to the search button in SearchItem component

--- online-booking-app/src/Pages/list/List.jsx
+++ online-booking-app/src/Pages/list/List.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../Components/navbar/Navbar";
 import Header from "../../Components/header/Header";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
-import { format } from "date-fns";
+import { format, max } from "date-fns";
 import { DateRange } from "react-date-range";
 import SearchItem from "../../Components/searchItems/SearchItems";
 import useFetch from "../../hooks/useFetch";
@@ -14,9 +14,17 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
+  
+  const { data, loading, error, reFetch } = useFetch(`/hotels?city=${destination}&min=${min || 0 }&max${max || 1400}`);
 
+  const handleClick = () => {
+    reFetch()
+  
+  }
 
-  const { data, loading, error, refetch } = useFetch(`/hotels?city=${destination}`)
+  
   return (
     <div>
       <Navbar />
@@ -50,13 +58,21 @@ const List = () => {
                   <span className="lsOptionText">
                     Min price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input 
+                    type="number" 
+                    onChange={e=>setMin(e.target.value)}
+                    className="lsOptionInput" 
+                  />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
                     Max price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input 
+                    type="number" 
+                    onChange={e=>setMax(e.target.value)}
+                    className="lsOptionInput" 
+                  />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Adult</span>
@@ -87,7 +103,7 @@ const List = () => {
                 </div>
               </div>
             </div>
-            <button>Search</button>
+            <button onClick={handleClick}>Search</button>
           </div>
           <div className="listResult">
             {loading ? (
